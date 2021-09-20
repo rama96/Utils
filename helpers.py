424ed5864fc447256af1dc4a1bfd9aab202980f8
RAMAMURTHI <ramamurthi@Administrators-MacBook-Pro-135.local>
2021-09-20 23:32:57 +0530

    Adding Missing Data Imputer

--- helpers.py
+++ helpers.py
@@ -6,17 +6,7 @@ import scipy.stats as stats
 import sklearn.linear_model as linear_model
 import seaborn as sns
 import xgboost as xgb
-from sklearn.experimental import enable_iterative_imputer  # noqa
-# now you can import normally from sklearn.impute
-from sklearn.impute import IterativeImputer
-from sklearn.ensemble import ExtraTreesRegressor
-#from sklearn.neighbors import KNeighborsRegressor
-from sklearn.preprocessing import OrdinalEncoder
-from sklearn.impute import SimpleImputer
 
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt 
 import seaborn as sns
 from scipy import stats
 from scipy.stats import norm, skew 
@@ -29,73 +19,10 @@ from scipy import stats
 from sklearn.preprocessing import LabelEncoder,OneHotEncoder
 from sklearn.preprocessing import PowerTransformer
 
-def missing_data_distribution(df):
-    missing = df.isnull().sum()
-    missing = missing[missing > 0]
-    missing = missing/len(df)
-    missing.sort_values(ascending = False, inplace=True)
-    missing = missing * 100
-    missing.plot.bar()
-    print(missing.head(20))
 
 
-def seperating_data(df):
-    numeric_data = df.select_dtypes(include=[np.number])
-    categorical_data = df.select_dtypes(exclude=[np.number])
-    print(numeric_data.head())
-    print(categorical_data.head())
-    return numeric_data,categorical_data
 #categorical_data.head()
 
-def numerical_impute(df):
-    my_imputer = SimpleImputer()
-    new_data_1 = df.copy()
-    my_imputer = SimpleImputer()
-    new_data_1 = pd.DataFrame(my_imputer.fit_transform(new_data_1))
-    new_data_1.columns = df.columns
-    new_data_1.index = df.index
-    missing_new = new_data_1.isnull().sum()
-    print(missing_new)
-    return new_data_1
-
-def encode(data):
-    '''function to encode non-null data and replace it in the original data'''
-    #retains only non-null values
-    nonulls = np.array(data.dropna())
-    #reshapes the data for encoding
-    impute_reshape = nonulls.reshape(-1,1)
-    #encode date
-    impute_ordinal = encoder.fit_transform(impute_reshape)
-    #Assign back encoded values to non-null values
-    data.loc[data.notnull()] = np.squeeze(impute_ordinal)
-    return data
-
-
-# %%
-def categorical_impute(df):
-    #instantiate both packages to use
-    encoder = OrdinalEncoder()
-    imputer = IterativeImputer(ExtraTreesRegressor())
-    #create a list of categorical columns to iterate over
-    cat_cols = df.columns
-
-    def encode(data):
-        '''function to encode non-null data and replace it in the original data'''
-        #retains only non-null values
-        nonulls = np.array(data.dropna())
-        #reshapes the data for encoding
-        impute_reshape = nonulls.reshape(-1,1)
-        #encode date
-        impute_ordinal = encoder.fit_transform(impute_reshape)
-        #Assign back encoded values to non-null values
-        data.loc[data.notnull()] = np.squeeze(impute_ordinal)
-        return data
-
-    #create a for loop to iterate through each column in the data
-    for columns in cat_cols:
-        encode(df[columns])
-    encode_data = pd.DataFrame(np.round(imputer.fit_transform(df)),columns = df.columns)
-    return encode_data
 
 
 # %%
---
+++ utils/missing_data.py
@@ -0,0 +1,70 @@
+import pandas as pd
+import numpy as np
+from sklearn.impute import SimpleImputer
+from sklearn.preprocessing import OrdinalEncoder
+from sklearn.experimental import enable_iterative_imputer  # noqa
+# now you can import normally from sklearn.impute
+from sklearn.impute import IterativeImputer
+from sklearn.ensemble import ExtraTreesRegressor
+#from sklearn.neighbors import KNeighborsRegressor
+
+class MissingDataHandler:
+    
+    def __init__(self,df) -> None:
+        self.df = df
+        self.show_distribution(self)
+        self.assign_data_labels()
+    
+    def show_distribution(self):
+        missing = self.df.isnull().sum()
+        missing = missing[missing > 0]
+        missing = missing/len(self.df)
+        missing.sort_values(ascending = False, inplace=True)
+        missing = missing * 100
+        missing.plot.bar()
+        print(missing.head(50))
+
+    def assign_data_labels(self):
+        self.numeric_data = self.df.select_dtypes(include=[np.number])
+        self.categorical_data = self.df.select_dtypes(exclude=[np.number])
+        print("Numerical Data : " , self.numeric_data.head())
+        print("Categorical Data : " , self.categorical_data.head())
+
+    def numerical_impute(self):
+        my_imputer = SimpleImputer()
+        _df = self.numeric_data.copy()
+        _df = pd.DataFrame(my_imputer.fit_transform(_df))
+        _df.columns = self.df.columns
+        _df.index = self.df.index
+        missing_new = _df.isnull().sum()
+        print(missing_new)
+        self.numeric_imputed_data = _df
+
+
+    # %%
+    def categorical_impute(self):
+        #instantiate both packages to use
+        encoder = OrdinalEncoder()
+        imputer = IterativeImputer(ExtraTreesRegressor())
+        #create a list of categorical columns to iterate over
+        cat_cols = self.categorical_data.columns
+
+        def encode(data):
+            '''function to encode non-null data and replace it in the original data'''
+            #retains only non-null values
+            nonulls = np.array(data.dropna())
+            #reshapes the data for encoding
+            impute_reshape = nonulls.reshape(-1,1)
+            #encode date
+            impute_ordinal = encoder.fit_transform(impute_reshape)
+            #Assign back encoded values to non-null values
+            data.loc[data.notnull()] = np.squeeze(impute_ordinal)
+            return data
+
+        #create a for loop to iterate through each column in the data
+        for columns in cat_cols:
+            encode(self.categorical_data[columns])
+        encode_data = pd.DataFrame(np.round(imputer.fit_transform(self.categorical_data)),columns = self.categorical_data.columns)
+        self.categorical_imputed_data = encode_data
+        
+
